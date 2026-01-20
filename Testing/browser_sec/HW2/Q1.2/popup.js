6a40f4d15726751dd90d01a7fcbfa3332571535f
Mitchell Zhou <mitchelz@andrew.cmu.edu>
2026-01-20 17:55:39 -0500

    Ignore Testing and CTFs/Testing Bench directories

--- Testing/browser_sec/HW2/Q1.2/popup.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const nameInput = document.getElementById("nameInput");
-    const greeting = document.getElementById("greeting");
-
-    nameInput.addEventListener("input", function () {
-        const name = nameInput.value.trim();
-        greeting.textContent = name ? `Hello ${name}!` : "";
-    });
-});
--- Testing/browser_sec/HW3/setup.js
+++
@@ -1,8 +0,0 @@
-const secret = "14-828";
-const vm = require('vm');
-
-let context = {};
-vm.createContext(context); // Contextify the object.
-
-code = "this.constructor.constructor('return secret')()";
-vm.runInContext(code, context);
